6b2de39038fba264591326087eff40d89d6f2466
Ege Bilecen <egebilecen@icloud.com>
2022-09-28 20:16:19 +0300

    added new method to logger to log exceptions

--- src/Logger.cs
+++ src/Logger.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 public static class Logger
 {
@@ -9,5 +10,17 @@ public static class Logger
         var file = File.AppendText(logFile);
         file.WriteLine(text);
         file.Close();
+    }
+
+    public static void LogException(Exception ex, string additional_msg="")
+    {
+        string ex_msg = "\n---------------\n" +
+                        "Exception: "+ex.GetType().FullName +
+                        "\nMessage: "+ex.Message +
+                        "\nStack trace: "+ex.StackTrace.Trim() +
+                        "\nDate: "+DateTime.Now.ToString("dd/MM/yyyy, HH:mm:ss") +
+                        (additional_msg != "" ? "\n"+additional_msg : "") +
+                        "\n---------------\n";
+        File.AppendAllText(logFile, ex_msg);
     }
 }
