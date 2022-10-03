0b2650a618f9d892a66439add93751ecf05df852
Ege Bilecen <egebilecen@icloud.com>
2022-10-03 20:29:07 +0300

    v1.2.2

--- src/Program.cs
+++ src/Program.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 public static class Application
 {
     public const string                botRepoURL      = "https://github.com/egebilecen/PZServerDiscordBot";
-    public const string                botVersion      = "v1.2.1";
+    public const string                botVersion      = "v1.2.2";
     public const float                 botVersionMajor = 1.2f;
 
     public static Settings.BotSettings botSettings;
