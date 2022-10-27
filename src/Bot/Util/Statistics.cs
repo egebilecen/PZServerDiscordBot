51d421c08896eb273cf4b19a15a41ab88a32eb28
Ege Bilecen <egebilecen@icloud.com>
2022-10-27 13:54:19 +0300

    added new command to README and added images

--- src/Bot/Util/Statistics.cs
+++ src/Bot/Util/Statistics.cs
@@ -47,7 +47,7 @@ public static class Statistics
         char fillChar = '#';
         int  barWidth = 20;
 
-        int repeatCount = ((int)(val - (val % 10))) / 10 * (barWidth / 10);
+        int repeatCount = ((int)(val - (val % 5))) / 5;
         string progressBar = string.Format("{0} [{1}] {2}%", title, new string(fillChar, repeatCount) + new string(padChar, barWidth - repeatCount), val.ToString("0.##"));
         
         return progressBar;
