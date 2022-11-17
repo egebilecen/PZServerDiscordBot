5c668a8566a81950b3c3e129219fe821d0bb1ac3
Ege Bilecen <egebilecen@icloud.com>
2022-11-18 00:35:09 +0300

    removed beta mark

--- src/Program.cs
+++ src/Program.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 public static class Application
 {
     public const string                    BotRepoURL = "https://github.com/egebilecen/PZServerDiscordBot";
-    public static readonly SemanticVersion BotVersion = new SemanticVersion(1, 5, 0, DevelopmentStage.Beta);
+    public static readonly SemanticVersion BotVersion = new SemanticVersion(1, 5, 0, DevelopmentStage.None);
     public static Settings.BotSettings     BotSettings;
 
     public static DiscordSocketClient  Client;
