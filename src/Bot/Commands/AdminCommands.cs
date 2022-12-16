19881aa462daa14d8e8a56fbc2b4544090158870
Ege Bilecen <egebilecen@icloud.com>
2022-12-16 20:38:11 +0300

    removed debug command content

--- src/Bot/Commands/AdminCommands.cs
+++ src/Bot/Commands/AdminCommands.cs
@@ -12,19 +12,5 @@ public class AdminCommands : ModuleBase<SocketCommandContext>
     [Remarks("skip")]
     public async Task Debug(string param1="", string param2="", string param3="")
     {
-        // I got some reports about bot missing some workshop updates.
-        // This command will print some debug information about that.
-        var fetchDetails = Task.Run(async () => await SteamWebAPI.GetWorkshopItemDetails(new string[] { param1 }));
-        var itemDetails  = fetchDetails.Result;
-
-        File.WriteAllText("./debug_result.json", JsonConvert.SerializeObject(itemDetails, Formatting.Indented));
-
-        await Context.Message.AddReactionAsync(EmojiList.GreenCheck);
-        await DiscordUtility.SendEmbeddedMessage(Context.Channel, new List<KeyValuePair<string, string>>
-        {
-            new KeyValuePair<string, string>("Application.startTime", Application.StartTime.ToString()),
-            new KeyValuePair<string, string>("Application.startTime (timestamp)", ((DateTimeOffset)Application.StartTime).ToUnixTimeSeconds().ToString()),
-        });
-        await Context.Channel.SendFileAsync("./debug_result.json");
     }
 }
