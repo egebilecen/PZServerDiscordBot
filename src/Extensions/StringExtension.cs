17074d1924a214d83506a04a88ebeedbefd26a70
Ege Bilecen <egebilecen@icloud.com>
2022-12-18 15:12:25 +0300

    added string extension class to format string based on keys

---
+++ src/Extensions/StringExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class StringExtension
+{
+    public static string Format_(this string str, params (string, string)[] formatPair)
+    {
+        if(formatPair.Length < 1) return str;
+
+        foreach((string, string) pair in formatPair)
+            str = str.Replace("{" + pair.Item1 + "}", pair.Item2);
+
+        return str;
+    }
+}
