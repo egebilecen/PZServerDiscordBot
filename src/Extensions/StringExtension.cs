b2f6cbde044480d4cc10f74504f388f30dc29b6e
Ege Bilecen <egebilecen@icloud.com>
2022-12-18 15:15:53 +0300

    updated extension format method's name to KeyFormat

--- src/Extensions/StringExtension.cs
+++ src/Extensions/StringExtension.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 public static class StringExtension
 {
-    public static string Format_(this string str, params (string, string)[] formatPair)
+    public static string KeyFormat(this string str, params (string, string)[] formatPair)
     {
         if(formatPair.Length < 1) return str;
 
