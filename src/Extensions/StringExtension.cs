8ff5b29cf3c724b57af7bd74837b6593a1f00d75
Ege Bilecen <egebilecen@icloud.com>
2022-12-18 15:21:40 +0300

    KeyFormat() method now accepts object and auto calls ToString()

--- src/Extensions/StringExtension.cs
+++ src/Extensions/StringExtension.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 public static class StringExtension
 {
-    public static string KeyFormat(this string str, params (string, string)[] formatPair)
+    public static string KeyFormat(this string str, params (string, object)[] formatPair)
     {
         if(formatPair.Length < 1) return str;
 
         foreach((string, string) pair in formatPair)
-            str = str.Replace("{" + pair.Item1 + "}", pair.Item2);
+            str = str.Replace("{" + pair.Item1 + "}", pair.Item2.ToString());
 
         return str;
     }
