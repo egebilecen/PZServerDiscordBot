b50f708f423f1a026e584ffdeb746ac2858aadab
Ege Bilecen <ege@r0m.cc>
2024-06-08 18:17:02 +0100

    v1.11.2

--- src/Program.cs
+++ src/Program.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 public static class Application
 {
     public const string                    BotRepoURL = "https://github.com/egebilecen/PZServerDiscordBot";
-    public static readonly SemanticVersion BotVersion = new SemanticVersion(1, 11, 1, DevelopmentStage.Release);
+    public static readonly SemanticVersion BotVersion = new SemanticVersion(1, 11, 2, DevelopmentStage.Release);
     public static Settings.BotSettings     BotSettings;
 
     public static DiscordSocketClient  Client;
