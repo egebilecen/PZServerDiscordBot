44fbbbbe9118468d1d9ed5a5af792289f61b9a6b
Ege Bilecen <ege@r0m.cc>
2024-07-22 15:01:19 +0100

    fix: !abort_restart ignoring timed restarts

--- src/Bot/Schedules/ServerRestart.cs
+++ src/Bot/Schedules/ServerRestart.cs
@@ -4,6 +4,15 @@ public static partial class Schedules
 {
     public static void ServerRestart(List<object> args)
     {
+        if(Application.BotSettings.ServerScheduleSettings.ServerRestartScheduleType.ToLower() == "time"
+        && ServerUtility.AbortNextTimedServerRestart)
+        {
+            ServerUtility.AbortNextTimedServerRestart = false;
+            ServerUtility.ResetServerRestartInterval();
+
+            return;
+        }
+
         bool isServerRunning = ServerUtility.IsServerRunning();
         var  publicChannel   = DiscordUtility.GetTextChannelById(Application.BotSettings.PublicChannelId);
         var  logChannel      = DiscordUtility.GetTextChannelById(Application.BotSettings.LogChannelId);
